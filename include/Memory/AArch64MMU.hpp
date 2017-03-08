880306318f79913517f23c842918b2455e8f15c1
Alexander Meißner <AlexanderMeissner@gmx.net>
2017-03-08 14:48:48 +0100

    Added MMU::initialize (but untested)

--- include/Memory/AArch64MMU.hpp
+++ include/Memory/AArch64MMU.hpp
@@ -1,11 +1,15 @@
 #include <Memory/AArch64Cache.hpp>
 
 namespace MMU {
+    const Natural64 attributePalette = 0x0000FFBB440C0400;
+
     enum AttributeIndex {
-        Device = 0,
-        NonCacheable = 1,
-        WriteThrough = 2,
-        WriteBack = 3
+        DeviceNGNRNE = 0,
+        DeviceNGNRE = 1,
+        DeviceGRE = 2,
+        NonCacheable = 3,
+        WriteThrough = 4, // Outer and inner, read-write-allocate
+        WriteBack = 5 // Outer and inner, read-write-allocate
     };
 
     enum Shareability {
@@ -39,13 +43,55 @@ namespace MMU {
     };
 
     enum Granularity {
-        Granule4 = 9,
-        Granule16 = 11,
-        Granule64 = 13
+        Granule4KiB = 9,
+        Granule16KiB = 11,
+        Granule64KiB = 13
     };
 
     template<Natural16 addressBits>
     struct PageTable {
         PageTableEntry entries[2<<addressBits];
     };
+
+    volatile Natural8* initialize() {
+        auto dram = AllwinnerDRAM::instances[0].address;
+        auto dramCom = AllwinnerDRAMCOM::instances[0].address;
+        auto dramEnd = reinterpret_cast<NativeNaturalType>(dram)+(1ULL<<dramCom->getDRAMSize());
+        auto level2Table = reinterpret_cast<volatile PageTable<Granule64KiB>*>(dramEnd-sizeof(PageTable<Granule64KiB>));
+
+        for(Natural32 index = 0; index < 8192; ++index) {
+            auto entry = &level2Table->entries[index];
+            entry->raw = 0x20000000*index;
+            entry->lastLevelAccessFlag = 1;
+        }
+
+        auto entry = &level2Table->entries[0];
+        entry->valid = 1;
+        entry->lastLevelAttributeIndex = DeviceNGNRE;
+        entry->lastLevelShareability = OuterShareable;
+
+        for(Natural32 index = 2; index < 6; ++index) {
+            entry = &level2Table->entries[index];
+            entry->valid = 1;
+            entry->lastLevelAttributeIndex = WriteBack;
+            entry->lastLevelShareability = InnerShareable;
+        }
+
+        asm volatile(
+            "dmb st\n"
+            "msr TTBR0_EL3, %x0\n"
+            "msr MAIR_EL3, %x1\n"
+            "msr TCR_EL3, %x2\n"
+            "isb\n" : : "r"(level2Table), "r"(attributePalette), "r"(
+            (64-32)| // Starting at level 2
+            (3<< 8)| // Inner cacheability: Normal memory, Inner Write-Back no Write-Allocate Cacheable
+            (3<<10)| // Outer cacheability: Normal memory, Outer Write-Back no Write-Allocate Cacheable
+            (3<<12)| // Shareability: Inner shareable
+            (1<<14)| // Granule size: 64 KiB
+            (0<<16)| // Physical address space: 4 GiB
+            (0<<20)  // Top byte used in the address calculation
+        ));
+
+        return reinterpret_cast<volatile Natural8*>(level2Table);
+    }
 };
